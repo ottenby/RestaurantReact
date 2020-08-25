10879829df00fc608191b31c00a50cf840d9a152
mariaekmaan1991 <mariaekmaan@hotmail.com>
2020-08-25 15:09:40 +0200

    ändra

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,25 +1,14 @@
 import React, { useState, useEffect } from "react";
-import { Query } from "../models/Query";
 import axios from 'axios';
 
-export class Booking {
-  _id: string = '';
-  guestName: string = '';
-  amountOfGuests: string = '';
-  customerId: string = '';
-  time: string = '';
-  date: string = ';'
-  bookingActive: boolean = false;
-  bookingFinished: boolean = false;
-}
 export interface IBooking {
 
   _id: string;
   guestName: string;
   amountOfGuests: string;
   customerId: string;
-  time: string,
-  date: string,
+  time: String,
+  date: String,
   bookingActive: boolean;
   bookingFinished: boolean;
 }
@@ -28,19 +17,13 @@ export interface IFormData {
   name: string;
   date: string;
   numberOfGuests: string;
-  phone: string;
-  email: string;
 }
-
-
 
 export function Form() {
   const [formData, setFormData] = useState<IFormData>({
     name: "",
     date: "",
-    numberOfGuests: "",
-    phone: "",
-    email: ""
+    numberOfGuests: ""
   });
 
   function updateFormValues(
@@ -52,108 +35,41 @@ export function Form() {
 
   console.log(formData);
 
-  function newQuery() {
-    let query = new Query();
-    query.date = formData.date;
-    query.numOfPeople = formData.numberOfGuests;
-    console.log(query);
-  }
-
-
-  function checkIfTable() {
-    let listOfBookingsSameDay = bookings.filter(booking => booking.date === formData.date);
-    console.log(listOfBookingsSameDay);
-
-    continueToCheck(listOfBookingsSameDay) 
-  }
-
-  let earlyBookings: IBooking[] = [];
-  let lateBookings: IBooking[] = [];
-
-  
-
-  function continueToCheck(array: IBooking[]) {
-
-    for (let i = 0; i < array.length; i++) {
-      
-      if(array[i].time === '18.00') {
-        earlyBookings.push(array[i])
-      } else {
-        lateBookings.push(array[i])
-      }
-      
-    }
-
-    console.log(earlyBookings)
-    console.log(lateBookings)
-    if (earlyBookings.length < 14) {
-      renderEarlyButton(earlyBookings) 
-    }
-
-    if (lateBookings.length < 14) {
-      // renderLateButton(lateBookings) 
-    }
-  }
-
-  function renderEarlyButton(bookings: IBooking[]) {
-    console.log('tidig')
-  }
-  function renderLateButton(bookings: IBooking[]) {
-    console.log('sen')
-  }
-
-  let aBooking = new Booking;
-  function newBooking(text: string) {
-
-    
-    aBooking.time = text;
-    aBooking.bookingFinished = false;
-    aBooking.bookingActive = true;
-    aBooking.amountOfGuests = formData.numberOfGuests;
-    aBooking.date = formData.date;
-    aBooking.guestName = formData.name;
-    // aBooking.customerId =
-
-    console.log(aBooking);
-    
-  }
-
 
   useEffect(()=> {
     axios.post('http://localhost:8000/', {
+      name: "Lovis"
+    })
+    .then(function () {
       
     })
-    .then(function (response) {
-      console.log(response.data)
-    })
   }, [])
-
+  
   let defaultValue: IBooking[] = [];
   const[bookings, setBookings] = useState(defaultValue);
-  console.log(bookings);
+
   useEffect(() => {
     axios.get("http://localhost:8000/").then(
         response => {
           let bookings: IBooking[] = response.data.map( (b: IBooking) => {
+            console.log(b)
             return {
-              id: b._id,
+              id: b._id, 
               numberOfGuests: b.amountOfGuests,
               guestId: b.customerId,
-              time: b.time,
+              sitting: b.time,
               date: b.date,
               active: b.bookingActive,
               finished: b.bookingFinished
             }
-          })
-
+          }) 
+          
           setBookings(bookings)
-
+          
         })
   }, [])
+  console.log(bookings);
 
-function showSecondForm() {
-
-}
 
 
   return (
@@ -179,31 +95,9 @@ function showSecondForm() {
         onChange={e => updateFormValues(e, "numberOfGuests")}
       />
       <button
-        onClick={checkIfTable}>
+        /*onClick={newQuery}*/>
         Check for available tables
       </button>
-
-      <div className="second-form">
-        <input
-          type="text"
-          name="mobile"
-          placeholder="Phone Number"
-          className="mobile"
-          onChange={e => updateFormValues(e, "phone")}
-        />
-        <input
-          type="text"
-          name="email"
-          placeholder="Email"
-          className="email"
-          onChange={e => updateFormValues(e, "email")}
-        />
-
-
-          <button onClick={()=> {newBooking('18.00')}}>18.00</button>
-          <button onClick={()=> {newBooking('20.30')}}>20.30</button>
-
-      </div>
     </>
   );
 }
