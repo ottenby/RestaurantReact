bea74bc2b5612b9bb746f655e764deb068e46f4e
mariaekmaan1991 <mariaekmaan@hotmail.com>
2020-08-25 17:38:32 +0200

    change the folder structure

--- src/views/ GuestView.tsx
+++ src/views/ GuestView.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Form } from '../components/from/Form';
-import { Tables } from '../components/guestTables/GusetTables';
+import { GuestTables } from '../components/guestTables/GusetTables';
 
 export function GuestView() {
 
     return(
         <React.Fragment>
             <Form></Form>
-            <Tables></Tables>
+            <GuestTables></GuestTables>
         </React.Fragment>
     )
 }
