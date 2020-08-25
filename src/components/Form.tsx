dd9e64418fa5b2c22234be6107efc728f58f95d9
Love Ottenby <ottenby123@gmail.com>
2020-08-25 14:02:16 +0200

    checkIfTable

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,15 +2,24 @@ import React, { useState, useEffect } from "react";
 import { Query } from "../models/Query";
 import axios from 'axios';
 
-
+export class Booking {
+  _id: string = '';
+  guestName: string = '';
+  amountOfGuests: string = '';
+  customerId: string = '';
+  time: string = '';
+  date: string = ';'
+  bookingActive: boolean = false;
+  bookingFinished: boolean = false;
+}
 export interface IBooking {
 
   _id: string;
   guestName: string;
   amountOfGuests: string;
   customerId: string;
-  time: String,
-  date: String,
+  time: string,
+  date: string,
   bookingActive: boolean;
   bookingFinished: boolean;
 }
@@ -19,13 +28,19 @@ export interface IFormData {
   name: string;
   date: string;
   numberOfGuests: string;
+  phone: string;
+  email: string;
 }
+
+
 
 export function Form() {
   const [formData, setFormData] = useState<IFormData>({
     name: "",
     date: "",
-    numberOfGuests: ""
+    numberOfGuests: "",
+    phone: "",
+    email: ""
   });
 
   function updateFormValues(
@@ -44,42 +59,101 @@ export function Form() {
     console.log(query);
   }
 
- 
+
+  function checkIfTable() {
+    let listOfBookingsSameDay = bookings.filter(booking => booking.date === formData.date);
+    console.log(listOfBookingsSameDay);
+
+    continueToCheck(listOfBookingsSameDay) 
+  }
+
+  let earlyBookings: IBooking[] = [];
+  let lateBookings: IBooking[] = [];
+
+  
+
+  function continueToCheck(array: IBooking[]) {
+
+    for (let i = 0; i < array.length; i++) {
+      
+      if(array[i].time === '18.00') {
+        earlyBookings.push(array[i])
+      } else {
+        lateBookings.push(array[i])
+      }
+      
+    }
+
+    console.log(earlyBookings)
+    console.log(lateBookings)
+    if (earlyBookings.length < 14) {
+      renderEarlyButton(earlyBookings) 
+    }
+
+    if (lateBookings.length < 14) {
+      // renderLateButton(lateBookings) 
+    }
+  }
+
+  function renderEarlyButton(bookings: IBooking[]) {
+    console.log('tidig')
+  }
+  function renderLateButton(bookings: IBooking[]) {
+    console.log('sen')
+  }
+
+  let aBooking = new Booking;
+  function newBooking(text: string) {
+
+    
+    aBooking.time = text;
+    aBooking.bookingFinished = false;
+    aBooking.bookingActive = true;
+    aBooking.amountOfGuests = formData.numberOfGuests;
+    aBooking.date = formData.date;
+    aBooking.guestName = formData.name;
+    // aBooking.customerId =
+
+    console.log(aBooking);
+    
+  }
+
 
   useEffect(()=> {
     axios.post('http://localhost:8000/', {
-      name: "Lovis"
-    })
-    .then(function (response) {
       
     })
+    .then(function (response) {
+      console.log(response.data)
+    })
   }, [])
-  
+
   let defaultValue: IBooking[] = [];
   const[bookings, setBookings] = useState(defaultValue);
-
+  console.log(bookings);
   useEffect(() => {
     axios.get("http://localhost:8000/").then(
         response => {
           let bookings: IBooking[] = response.data.map( (b: IBooking) => {
-            console.log(b)
             return {
-              id: b._id, 
+              id: b._id,
               numberOfGuests: b.amountOfGuests,
               guestId: b.customerId,
-              sitting: b.time,
+              time: b.time,
               date: b.date,
               active: b.bookingActive,
               finished: b.bookingFinished
             }
-          }) 
-          
+          })
+
           setBookings(bookings)
-          
+
         })
   }, [])
-  console.log(bookings);
 
+function showSecondForm() {
+
+}
 
 
   return (
@@ -105,9 +179,31 @@ export function Form() {
         onChange={e => updateFormValues(e, "numberOfGuests")}
       />
       <button
-        onClick={newQuery}>
+        onClick={checkIfTable}>
         Check for available tables
       </button>
+
+      <div className="second-form">
+        <input
+          type="text"
+          name="mobile"
+          placeholder="Phone Number"
+          className="mobile"
+          onChange={e => updateFormValues(e, "phone")}
+        />
+        <input
+          type="text"
+          name="email"
+          placeholder="Email"
+          className="email"
+          onChange={e => updateFormValues(e, "email")}
+        />
+
+
+          <button onClick={()=> {newBooking('18.00')}}>18.00</button>
+          <button onClick={()=> {newBooking('20.30')}}>20.30</button>
+
+      </div>
     </>
   );
 }
