49db8f9d0775123826ba258592bb5512da75b2aa
mariaekmaan1991 <mariaekmaan@hotmail.com>
2020-08-26 23:01:11 +0200

    radera

--- src/views/ GuestView.tsx
+++ src/views/ GuestView.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Tables } from '../components/admin/tables/Tables';
-import { GuestParent } from '../components/guest/GuestParent';
+
 
 export function GuestView() {
 
     return(
         <React.Fragment>
-            <GuestParent></GuestParent>
+
             <Tables></Tables>
         </React.Fragment>
     )
