0c9c5442486129d5584e9f07ec6aca1f436f39d5
ullagulla <ulrikaa91@hotmail.com>
2020-09-10 11:33:20 +0200

    removed unused functions

--- src/components/admin/tables/Tables.tsx
+++ src/components/admin/tables/Tables.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, useState } from 'react';
+import React, { useState } from 'react';
 import { IBooking } from '../AdminParent';
 import { IGuest } from '../../guest/GuestParent';
 import axios from 'axios';
